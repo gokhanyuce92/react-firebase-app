f1f724cf6a0bc5a7bec2079c8e51614f67d33e79
gokhanyuce <g.yuce92@gmail.com>
2025-08-27 13:26:13 +0300

    gitignore ve vite config güncellendi.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/",
     plugins: [react()],
     server: {
         headers: {
             "Cross-Origin-Opener-Policy": "unsafe-none",
         },
+    },
+    build: {
+        outDir: "dist",
     },
 });
